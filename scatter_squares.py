280ee5fd4922b211fc151ed7cdb8aacfe2edc3bb
Ambujse <blastedaway94.com>
2025-11-13 06:59:08 +0000

    B: New kinds of graphs

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 #Set chart title and label axes.
 ax.set_title("Square Numbers",fontsize=24)
@@ -11,5 +14,9 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 #Set size of tick labels.
 ax.tick_params(labelsize=14)
+
+#Set the range for each axis.
+ax.axis([0, 1100, 0, 1100000])
+ax.ticklabel_format(style='plain')
 
 plt.show()
