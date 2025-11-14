209059960e07f9a16e11c33c8542260ce60f26b7
Ambujse <blastedaway94.com>
2025-11-14 05:49:23 +0000

    E: corrected the while loop

--- rw_visual.py
+++ rw_visual.py
@@ -4,7 +4,9 @@ from random_walk import *
 
 # Keep making new walks, as long as the program is active.
 
-while True:
+keep_running = True
+
+while keep_running:
 
     # Make a random walk.
     rw = RandomWalk()
@@ -13,10 +15,19 @@ while True:
     # Plot the points in the walk.
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, 
+                edgecolors='none', s=15)
+    # ax.scatter(rw.x_values, rw.y_values, s=15)
     ax.set_aspect('equal')
     plt.show()
 
-    keep_running = input("Make another walk? (y/n):")
-    if keep_running == 'n':
-        break
+    while True:
+        user_input = input("Make another walk? (y/n): ")
+        if user_input == 'y':
+            break
+        elif user_input == 'n':
+            keep_running = False
+            break
+        else:
+            print("Wrong input. Please enter 'y' or 'n'.")
